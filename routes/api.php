b577c1b4fe3006848593b15328181516c03f6a1c
JeanMurgas <jeancamurgas30@gmail.com>
2023-11-04 13:19:37 -0500

    ajustes de rama

--- routes/api.php
+++ routes/api.php
@@ -19,24 +19,6 @@ use App\Http\Controllers\InventarioController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-###########################################
-########    RUTAS DE RECLAMOS    ##########
-###########################################
-
-Route::post('/api/reclamo', [Reclamo::class, 'guardarReclamo']);
-Route::get('/api/reclamo',  [Reclamo::class, 'getAllReclamos']);
-Route::get('/api/reclamo/estados',      [Reclamo::class, 'getAllEstados']);
-Route::get('/api/reclamo/categorias',   [Reclamo::class, 'getAllCategorias']);
-Route::get('/api/reclamo/prioridades',  [Reclamo::class, 'getAllPrioridades']);
-Route::get('/api/reclamo/{reclamo_id}',         [Reclamo::class, 'getReclamoById']);
-Route::get('/api/reclamo/cliente/{cliente_id}', [Reclamo::class, 'getReclamosCliente']);
-Route::patch('/api/reclamo/{reclamo_id}/estado',    [Reclamo::class, 'updateEstado']);
-Route::patch('/api/reclamo/{reclamo_id}/prioridad', [Reclamo::class, 'updatePrioridad']);
-
-Route::post('api/sugerencia',   [Sugerencia::class, 'guardarSugerencia']);
-Route::get('api/sugerencia',    [Sugerencia::class, 'getSugerencia']);
-
 #RUTAS MODULO 1
 Route::get('/api/inventario', [InventarioController::class, 'VerInventario']);
 Route::post('/api/agregar-marca', [AgregarMarcaController::class, 'GuardarMarca']);
