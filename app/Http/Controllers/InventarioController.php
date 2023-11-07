7d63683ce3c93ea81edb6353b0a9ba10292c257d
JeanMurgas <jeancamurgas30@gmail.com>
2023-11-07 17:54:20 -0500

    update se agregaron el punto de reorden y ver los productos por filtros

--- app/Http/Controllers/InventarioController.php
+++ app/Http/Controllers/InventarioController.php
@@ -9,22 +9,48 @@ use App\Models\Producto;
 class InventarioController extends Controller
 {
     public function VerInventario(Request $request)
-{
-    // Realiza las validaciones
-    $request->validate([
-        // Debe haber productos para mostrar
-        'productos' => 'required',
-        // Agregar la validación personalizada para "No puede bajar de un mínimo de cantidad de producto" aquí.
-    ]);
+    {
+        // Realiza las validaciones
+        $request->validate([
+            // Debe haber productos para mostrar
+            'productos' => 'required',
+        ]);
 
-    // Consulta los productos y devuelve una respuesta JSON
-    $productos = Producto::all();
+        // 1. Validación para mostrar un punto de reorden
+        // Comprueba si hay algún producto cuya cantidad en stock esté por debajo del punto de reorden.
+        $productosPuntoReorden = Producto::where('cantidad_stock', '<', 'punto_reorden')->get();
 
-    if ($productos->isEmpty()) {
-        return response()->json(['message' => 'No hay productos para mostrar.'], 404);
+        if ($productosPuntoReorden->isEmpty()) {
+            return response()->json(['message' => 'No hay productos por debajo del punto de reorden.'], 200);
+        }
+
+        // 2. Filtros para buscar productos específicos
+        $filtroNombre = $request->input('nombre');
+        $filtroCategoria = $request->input('categoria');
+
+        // Consulta los productos con los filtros si es que son pedidos
+        $query = Producto::query();
+
+        if (!empty($filtroNombre)) {
+            $query->where('nombre', 'like', '%' . $filtroNombre . '%');
+        }
+
+        if (!empty($filtroCategoria)) {
+            $query->where('categoria', $filtroCategoria);
+        }
+
+        $productosFiltrados = $query->get();
+
+        if ($productosFiltrados->isEmpty()) {
+            return response()->json(['message' => 'No se encontraron productos con los filtros especificados.'], 200);
+        }
+
+        $productos = Producto::all();
+
+        if ($productos->isEmpty()) {
+            return response()->json(['message' => 'No hay productos para mostrar.'], 404);
+        }
+
+        return response()->json($productos, 200);
     }
-
-    return response()->json($productos, 200);
-}
-
 }
