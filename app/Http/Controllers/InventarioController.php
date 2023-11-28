98e57ed52a1f1ce5e125ad20c96fe712d40416f0
Camilo Yepes <112560489+CamiloYeps@users.noreply.github.com>
2023-11-28 18:23:18 -0500

    InventarioController modificado

--- app/Http/Controllers/InventarioController.php
+++ app/Http/Controllers/InventarioController.php
@@ -7,57 +7,55 @@ use Illuminate\Http\Request;
 use App\Models\Producto;
 use Exception;
 
-class InventarioController extends Controller
-{
+class InventarioController extends Controller{
 
     //muestra todo el inventario
     public function verInventario(Request $request){
-    // Consulta los productos con relaciones cargadas
-    $productos = Producto::join('marca', 'producto.marca_id', '=', 'marca.id_marca')
-    ->join('categoria', 'producto.categoria_id', '=', 'categoria.id_categoria')
-    ->select('categoria.nombre as Categoria', 'marca.nombre as Marca', 'producto.id_producto','producto.nombre', 'producto.precio_unit', 'producto.cantidad_por_caja', 'producto.foto', 'producto.punto_reorden', 'producto.cantidad_caja')
-    ->get();
+        // Consulta los productos con relaciones cargadas
+        $productos = Producto::join('marca', 'producto.marca_id', '=', 'marca.id_marca')
+        ->join('categoria', 'producto.categoria_id', '=', 'categoria.id_categoria')
+        ->select('categoria.nombre as Categoria', 'marca.nombre as Marca', 'producto.id_producto','producto.nombre', 'producto.precio_unit', 'producto.cantidad_por_caja', 'producto.foto', 'producto.punto_reorden', 'producto.cantidad_caja')
+        ->get();
 
-    if ($productos->isEmpty()) {
-        return response()->json(['message' => 'No hay productos para mostrar.'], 404);
+        if ($productos->isEmpty()) {
+            return response()->json(['message' => 'No hay productos para mostrar.'], 404);
+        }
+
+        return response()->json($productos, 200);
     }
-
-    return response()->json($productos, 200);
-}
 
     //trae un producto en especifico
     public function buscarProductos(Request $request){
-    // Validaciones para los parámetros de búsqueda
-    $request->validate([
-        'nombre' => 'string',
-        'categoria' => 'integer',
-    ]);
+        // Validaciones para los parámetros de búsqueda
+        $request->validate([
+            'nombre' => 'string',
+            'categoria' => 'integer',
+        ]);
 
-    try {
-        // Obtén los parámetros de búsqueda del formulario
-        $nombre = $request->input('nombre');
-        $categoria = $request->input('categoria');
+        try {
+            // Obtén los parámetros de búsqueda del formulario
+            $nombre = $request->input('nombre');
+            $categoria = $request->input('categoria');
 
-        // Consulta de productos con filtros
-        $query = Producto::query();
+            // Consulta de productos con filtros
+            $query = Producto::query();
 
-        if (!empty($nombre)) {
-            $query->where('nombre', 'like', '%' . $nombre . '%');
+            if (!empty($nombre)) {
+                $query->where('nombre', 'like', '%' . $nombre . '%');
+            }
+
+            if (!empty($categoria)) {
+                $query->where('categoria_id', $categoria);
+            }
+
+            $productosFiltrados = $query->get();
+
+            // Devuelve los resultados de la búsqueda como respuesta JSON
+            return response()->json(['productos' => $productosFiltrados], 200);
+        } catch (Exception $e) {
+            // Manejar cualquier error y devolver una respuesta de error
+            return response()->json(['error' => 'Error al procesar la solicitud.'], 500);
         }
-
-        if (!empty($categoria)) {
-            $query->where('categoria_id', $categoria);
-        }
-
-        $productosFiltrados = $query->get();
-
-        // Devuelve los resultados de la búsqueda como respuesta JSON
-        return response()->json(['productos' => $productosFiltrados], 200);
-    } catch (Exception $e) {
-        // Manejar cualquier error y devolver una respuesta de error
-        return response()->json(['error' => 'Error al procesar la solicitud.'], 500);
     }
-}
-
 }
 
