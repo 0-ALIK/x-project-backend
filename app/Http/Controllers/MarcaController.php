a0dab8598139414b050990a47e53a4a1f8761085
Camilo Yepes <112560489+CamiloYeps@users.noreply.github.com>
2023-11-27 19:04:11 -0500

    MarcaController Modificado

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Marca;
+use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class MarcaController extends Controller
 {
@@ -22,52 +24,73 @@ class MarcaController extends Controller
 
     //agrega una marca
     public function guardarMarca(Request $request){
-    // Validar la solicitud
-    $request->validate([
-        'nombre' => 'required|unique:marca',
-        'descripcion' => 'required',
-        'logo' => 'required',
-    ]);
+        // Validar la solicitud
+        $request->validate([
+            'nombre' => 'required|unique:marca',
+            'descripcion' => 'required',
+            'logo' => 'required|image|mimes:png,jpg',
+        ]);
 
-    try {
-        // Verificar si la marca ya existe
-        $nombre = $request->input('nombre');
-        $marcaExistente = Marca::where('nombre', $nombre)->first();
+        try {
+            // Verificar si la marca ya existe
+            $nombre = $request->input('nombre');
+            $marcaExistente = Marca::where('nombre', $nombre)->first();
 
-        if ($marcaExistente) {
-            return response()->json(['error' => 'Ya existe una marca con el mismo nombre.'], 400);
+            if ($marcaExistente) {
+                return response()->json(['error' => 'Ya existe una marca con el mismo nombre.'], 400);
+            }
+
+            // Verificar si la imagen es válida
+            if (!$request->file('logo')->isValid()) {
+                return response()->json(['error' => 'Archivo de imagen no válido.'], 400);
+            }
+
+            // Verificar el tipo de archivo
+            $extension = $request->file('logo')->getClientOriginalExtension();
+            if ($extension != 'jpg' && $extension != 'png') {
+                return response()->json(['error' => 'Tipo de archivo no soportado. Solo se permiten archivos jpg y png.'], 400);
+            }
+
+            //crea una carpeta con el nombre marca_logo
+            if (!Storage::disk('public')->exists('marca_logo')){
+                Storage::disk('public')->makeDirectory('marca_logo');
+            }
+
+            // almacena el archivo en la carpeta 'marca_logo'
+            $result = $request->file('logo')->storeOnCloudinary('marca_logo');
+
+            // Crear una nueva marca
+            $nuevaMarca = new Marca;
+            $nuevaMarca->nombre = $nombre;
+            $nuevaMarca->descripcion = $request->input('descripcion');
+            $nuevaMarca->logo = $result->getSecurePath(); //obtiene la ruta en donde esta almacenado el archivo
+            $nuevaMarca->save();
+
+            // Devolver una respuesta exitosa
+            return response()->json(['message' => 'Marca creada con éxito.'], 201);
+        } catch (Exception $e) {
+            print($e);
+            return response()->json(['error' => 'Error al procesar la solicitud.'], 500);
         }
-
-        // Crear una nueva marca
-        $nuevaMarca = new Marca;
-        $nuevaMarca->nombre = $nombre;
-        $nuevaMarca->descripcion = $request->input('descripcion');
-        $nuevaMarca->logo = $request->input('logo');
-        $nuevaMarca->save();
-
-        // Devolver una respuesta exitosa
-        return response()->json(['message' => 'Marca creada con éxito.'], 201);
-    } catch (Exception $e) {
-        print($e);
-        return response()->json(['error' => 'Error al procesar la solicitud.'], 500);
     }
-}
 
     //obtiene una marca en especifico
     public function getMarca($id_marca) {
         try {
             $marca = Marca::findOrFail($id_marca);
-    
+
             return response()->json(["data" => $marca, "status" => 200]);
         } catch (Exception $e) {
             return response()->json(["mensaje" => "Error al obtener la marca", "status" => 500]);
         }
-    } 
-    
+    }
+
     //actualiza la informacion de una marca en especifico
     public function updateMarca(Request $request, $id_marca) {
         try {
             $marca = Marca::find($id_marca);
+
+            if ($request->hasfile)
 
             //verifica que la marca exista
             if (!$marca) {
