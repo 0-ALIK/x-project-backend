81c9d10fccb407486878c1e724014c675d62e077
Camilo Yepes <112560489+CamiloYeps@users.noreply.github.com>
2023-11-29 21:12:02 -0500

    importando migraciones

--- database/migrations/2023_10_25_180951_create_producto_table.php
+++ database/migrations/2023_10_25_180951_create_producto_table.php
@@ -6,33 +6,28 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('producto', function (Blueprint $table) {
             $table->id('id_producto');
-            $table->unsignedBigInteger('marca_id');
-            $table->unsignedBigInteger('categoria_id');
-            $table->string('nombre');
-            $table->decimal('precio_unit', 10, 2);
-            $table->integer('cantidad_por_caja');
+            $table->unsignedBigInteger('marca_id'); //FK
+            $table->unsignedBigInteger('categoria_id'); //FK
+            $table->unsignedInteger('cantidad_por_caja'); 
+            $table->unsignedInteger('cantidad_cajas'); 
+            $table->integer('punto_reorden'); 
+            $table->double('precio_unit'); 
+            $table->string('nombre'); 
             $table->string('foto');
-            $table->integer('punto_reorden');
-            $table->integer('cantidad_cajas');
 
             $table->timestamps();
 
-            //Foreign Key constraints
-            $table->foreign('marca_id')->references('id_marca')->on('marca');
+            // FOREIGN KEYS
             $table->foreign('categoria_id')->references('id_categoria')->on('categoria');
+            $table->foreign('marca_id')->references('id_marca')->on('marca');
+
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('producto');
